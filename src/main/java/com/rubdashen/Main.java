c31eb0b51dec679f8ceff01257ade28c3f85bd72
RubDaShen <142699406+RubDaShen@users.noreply.github.com>
2025-11-04 19:30:00 -0500

    first version omg

--- src/main/java/com/rubdashen/Main.java
+++ src/main/java/com/rubdashen/Main.java
@@ -1,9 +1,91 @@
 package com.rubdashen;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-public class Main {
-    public static void main(String[] args) {
+import com.rubdashen.tools.reader.FileReader;
+import org.jetbrains.annotations.NotNull;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
+public class Main {
+    static final Map<String, Runnable> commands = Map.of(
+        "create", Main::runCreateCommand
+    );
+
+    private static void runCreateCommand(@NotNull String action) {
+
+    }
+
+    public static void main(String[] args) {
+        final FileReader fileReader = new FileReader();
+
+        if (!fileReader.readFile("C:\\Users\\mallm\\Desktop\\Riinso\\assets\\code.rin")) {
+            System.out.println("Failed to read the file.");
+            return;
+        }
+
+        final String content = fileReader.getFileContent();
+        System.out.println(content);
+
+        if (content == null) {
+            System.out.println("File content is null.");
+            return;
+        }
+        if (content.isEmpty()) {
+            System.out.println("File content is empty.");
+            return;
+        }
+
+        final int size = content.length();
+
+        StringBuilder token = new StringBuilder();
+        boolean commandStart = false;
+        List<String> tokens = new ArrayList<>();
+
+        for (int i = 0; i < size; ++i) {
+            final char currentChar = content.charAt(i);
+
+            if (!commandStart) {
+                if (currentChar == '/') {
+                    System.out.println("Command start detected...");
+                    commandStart = true;
+                }
+
+                continue;
+            }
+
+            if (currentChar == '\n') {
+                tokens.add(token.toString());
+                token.setLength(0);
+
+                System.out.println("New line detected, command completed. Processing command...");
+
+                System.out.print("Tokens: ");
+                for (String t : tokens) {
+                    System.out.println(t + " ");
+                }
+
+                for (final String t : tokens) {
+                    if (commands.containsKey(t)) {
+                        System.out.println("Executing command: " + t);
+                        commands.get(t).run();
+                    }
+                    else {
+                        System.out.println("Unknown command: " + t);
+                    }
+                }
+
+                commandStart = false;
+                continue;
+            }
+
+            if (currentChar != ' ') {
+                token.append(currentChar);
+            }
+            else {
+                tokens.add(token.toString());
+                token.setLength(0);
+            }
+        }
     }
 }
--- src/main/java/com/rubdashen/tools/reader/FileReader.java
+++ src/main/java/com/rubdashen/tools/reader/FileReader.java
@@ -1,4 +1,50 @@
 package com.rubdashen.tools.reader;
 
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
+
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Path;
+
 public class FileReader {
+//	|-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-|
+//				    Members and Fields
+//	|-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-|
+
+    //	-------------------------------------------
+    //			        Variables
+    //	-------------------------------------------
+    private @Nullable String mFileContent;
+
+//	|-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-|
+//			        Functions and Methods
+//	|-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-|
+
+    //	-------------------------------------------
+    //			        Constructors
+    //	-------------------------------------------
+    public FileReader() { }
+
+    //	-------------------------------------------
+    //			        Functions
+    //	-------------------------------------------
+    public boolean readFile(@NotNull String filePath) {
+        final Path path = Path.of(filePath);
+        try {
+            this.mFileContent = Files.readString(path, StandardCharsets.UTF_8);
+            return true;
+        }
+        catch (IOException e) {
+            return false;
+        }
+    }
+
+    //	-------------------------------------------
+    //			    Setters and Getters
+    //	-------------------------------------------
+    public @Nullable String getFileContent() {
+        return this.mFileContent;
+    }
 }
